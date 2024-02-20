1e1246dec86669dbfdc6b2430a663aaea9a3a820
jesusddd23 <dossierjesus@gmail.com>
2024-02-20 10:11:32 -0400

    qq

---
+++ java script-Seccion-1/index.js
@@ -0,0 +1,83 @@
+console.log ("hola mundo soy jesus")
+
+var variableA= "jesus davila"
+console.log( "El nombre de el es:" + variableA );
+
+let nombre = " JESUS DAVILA "
+
+//Reasignando los valores de las variables usando prompt()
+// prompt(): es una funcion que nos permite introducir datos por teclado.
+//nombre = prompt("Ingresa tu nombre");
+//saldo = prompt("Ingresa el saldo que deseas");
+
+// Mostramos los datos introducidos por el usuario que se almacenan en las variables ya existentes (reasignacion)
+// console.log("Bienvenido " + nombre + " tu saldo es: " + saldo + "$");
+//alert("Bienvenido " + nombre + " tu saldo es: " + saldo + "$");
+
+
+
+// Raealizamos una calculadora utilizando variables de tipo let y prompt() para introducir datos por teclado.
+// adicionalmente mostramos el resultado en consola.
+
+//let num1 = +prompt("Ingresa un numero");
+//let num2 = +prompt("Ingresa otro numero");
+
+//console.log("total: " + (num1 + num2));
+
+
+
+
+//const: al igual que let es te alcance(scope) limitado, no se puede mutar, ni reasignar.
+const correo = "jesusdaniel@gmail.com"
+console.log(correo)
+
+// Intentamos reasignar la variable de tipo constante "correo"
+// correo = "correo@gmail.com"
+// console.log(correo)
+
+
+
+
+/* Ejercicio, agregar IVA a un monto */
+
+//let monto = +prompt("Ingresa un monto");
+//const IVA = 0.16;
+//let montoFinal = monto + (monto * IVA);
+
+//alert("El monto total a pagar es: " + montoFinal);
+
+
+
+
+
+/* 
+    EJERCICIO PARA EL DIA JUEVES 15 DE FEBRERO DE 2024
+    Realiza un programa que calcule el IMC de una persona. 
+*/
+
+// Utilizando prompt, y variables let y constantes.
+// Formula del IMC: peso / (altura * altura)
+
+//let peso = +prompt("Ingrese su peso actual");
+//let altura = +prompt("Ingrese su altura actual");
+//const IMC = (peso / (altura * altura)).toFixed(2);
+
+//alert ("Su IMC personal es :" + IMC); 
+
+
+//calculadora basica 
+
+const numero1 = +prompt ("ingrese el numero 1");
+const numero2 = +prompt ("ingrese el numero 2");
+const TOTAL = numero1 + numero2;
+
+alert ("el total es la suma es:" + TOTAL);
+    
+    /* 
+    + suma
+    - resta
+    * multiplicacion
+    / division
+    ** potencia
+*/
+
